0b687532dcca0d511a25817831d40fd01cca69d5
Alexey Krusiyan <aakrusiyan@gmail.com>
2024-09-23 15:17:38 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-example = 'Топинамбур'
-print (example[0])
-print (example[-1])
-print (example[5:10])
-print (example[::-1])
-print (example[1::2])
+task_count = 12
+hours_spent = 1.5
+course_name = 'Python'
+time_per_task = hours_spent / float(task_count)
+print('Курс:',course_name,'всего задач:',task_count,'затрачено часов:',hours_spent,'среднее время выполнения',time_per_task)
